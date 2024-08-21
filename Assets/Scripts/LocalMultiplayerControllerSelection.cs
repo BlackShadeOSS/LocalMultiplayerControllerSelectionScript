a1faba4b064e31362c81008b7da1316234a981d4
BlackShade <diaxsio10@gmail.com>
2024-08-21 20:55:32 +0200

    Tried to refactor, broken the blolking mechanic

--- Assets/Scripts/LocalMultiplayerControllerSelection.cs
+++ Assets/Scripts/LocalMultiplayerControllerSelection.cs
@@ -62,53 +62,123 @@ public class LocalMultiplayerControllerSelection : MonoBehaviour
     }
     
     // Update the toggle group options
+    // private void UpdateToggleGroupOptions()
+    // {
+    //     for (int i = 0; i < playerToggleGroups.Length; i++)
+    //     {
+    //         var toggleGroup = playerToggleGroups[i];
+    //         var playerNumber = int.Parse(GetNumbers(toggleGroup.name));
+    //         foreach (Transform child in toggleGroup.transform)
+    //         {
+    //             DestroyImmediate(child.gameObject);
+    //         }
+    //         for (int j = 0; j < AvailableInputDevices.Length; j++)
+    //         {   
+    //             //checking if there is already a toggle for this device
+    //             var toggles = toggleGroup.GetComponentsInChildren<Toggle>();
+    //             bool exists = false;
+    //             foreach (var togggle in toggles)
+    //             {
+    //                 if (togggle.GetComponentInChildren<Text>().text == AvailableInputDevices[j].displayName)
+    //                 {
+    //                     Debug.Log("Toggle for device: " + AvailableInputDevices[j].displayName + " already exists");
+    //                     exists = true;
+    //                 }
+    //             }
+    //             if (exists)
+    //             {
+    //                 continue;
+    //             }
+    //             var inputDevice = AvailableInputDevices[j];
+    //             var toggle = Instantiate(exampleToggle, toggleGroup.transform);
+    //             toggle.GetComponent<Toggle>().group = toggleGroup.GetComponent<ToggleGroup>();
+    //             toggle.GetComponentInChildren<Text>().text = inputDevice.displayName;
+    //             var index = j;
+    //             Debug.Log("Current index: " + j + " Device: " + inputDevice.displayName + " Player: " + playerNumber + " ToggleGroup: " + toggleGroup.name);
+    //             toggle.SetActive(true);
+    //             toggle.transform.localPosition = new Vector2(0, -30 * j);
+    //             if (SelectedDevices[playerNumber - 1] == inputDevice)
+    //             {
+    //                 toggle.GetComponent<Toggle>().isOn = true;
+    //                 Debug.Log("Setting toggle for device: " + inputDevice.displayName + " to true for player: " + playerNumber + "as it was previously selected");
+    //             }
+    //             else
+    //             {
+    //                 toggle.GetComponent<Toggle>().isOn = false;
+    //             }
+    //             toggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => OnToggleValueChanged(index, playerNumber, value));
+    //         }
+    //     }
+    // }
+
     private void UpdateToggleGroupOptions()
     {
-        for (int i = 0; i < playerToggleGroups.Length; i++)
+        for (int i = 0; i < playerToggleGroups.Length; i++) 
         {
-            var toggleGroup = playerToggleGroups[i];
-            var playerNumber = int.Parse(GetNumbers(toggleGroup.name));
-            foreach (Transform child in toggleGroup.transform)
-            {
-                DestroyImmediate(child.gameObject);
-            }
-            for (int j = 0; j < AvailableInputDevices.Length; j++)
-            {   
-                //checking if there is already a toggle for this device
-                var toggles = toggleGroup.GetComponentsInChildren<Toggle>();
-                bool exists = false;
-                foreach (var togggle in toggles)
+                var toggleGroup = playerToggleGroups[i];
+                var playerNumber = int.Parse(GetNumbers(toggleGroup.name));
+                if (toggleGroup.transform.childCount == 0)
                 {
-                    if (togggle.GetComponentInChildren<Text>().text == AvailableInputDevices[j].displayName)
+                    Debug.Log("No children found for player: " + playerNumber);
+                    Debug.Log("setting up toggle group for player: " + playerNumber);
+                    for (int j = 0; j < AvailableInputDevices.Length; j++)
                     {
-                        Debug.Log("Toggle for device: " + AvailableInputDevices[j].displayName + " already exists");
-                        exists = true;
+                        CreateToggleForDevice(AvailableInputDevices[j], playerNumber);
                     }
-                }
-                if (exists)
-                {
-                    continue;
-                }
-                var inputDevice = AvailableInputDevices[j];
-                var toggle = Instantiate(exampleToggle, toggleGroup.transform);
-                toggle.GetComponent<Toggle>().group = toggleGroup.GetComponent<ToggleGroup>();
-                toggle.GetComponentInChildren<Text>().text = inputDevice.displayName;
-                var index = j;
-                Debug.Log("Current index: " + j + " Device: " + inputDevice.displayName + " Player: " + playerNumber + " ToggleGroup: " + toggleGroup.name);
-                toggle.SetActive(true);
-                toggle.transform.localPosition = new Vector2(0, -30 * j);
-                if (SelectedDevices[playerNumber - 1] == inputDevice)
-                {
-                    toggle.GetComponent<Toggle>().isOn = true;
-                    Debug.Log("Setting toggle for device: " + inputDevice.displayName + " to true for player: " + playerNumber + "as it was previously selected");
                 }
                 else
                 {
-                    toggle.GetComponent<Toggle>().isOn = false;
+                    foreach (Transform child in toggleGroup.transform)
+                    {
+                        bool isDeviceAvailable = false;
+                        for (int j = 0; j < AvailableInputDevices.Length; j++)
+                        {
+                            if (child.GetComponentInChildren<Text>().text == AvailableInputDevices[j].displayName)
+                            {
+                                isDeviceAvailable = true;
+                                break;
+                            }
+                        }
+
+                        if (!isDeviceAvailable)
+                        {
+                            Destroy(child.gameObject);
+                        }
+                    }
+
+                    for (int j = 0; j < AvailableInputDevices.Length; j++)
+                    {
+                        bool isDeviceAvailable = false;
+                        for (int k = 0; k < toggleGroup.transform.childCount; k++)
+                        {
+                            if (toggleGroup.transform.GetChild(k).GetComponentInChildren<Text>().text ==
+                                AvailableInputDevices[j].displayName)
+                            {
+                                isDeviceAvailable = true;
+                                break;
+                            }
+                        }
+
+                        if (!isDeviceAvailable)
+                        {
+                            CreateToggleForDevice(AvailableInputDevices[j], playerNumber);
+                        }
+                    }
                 }
-                toggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => OnToggleValueChanged(index, playerNumber, value));
-            }
         }
+    }
+    
+    private void CreateToggleForDevice(InputDevice inputDevice, int playerNumber)
+    {
+        var toggleGroup = playerToggleGroups[playerNumber - 1];
+        var toggle = Instantiate(exampleToggle, toggleGroup.transform);
+        toggle.GetComponent<Toggle>().group = toggleGroup.GetComponent<ToggleGroup>();
+        toggle.GetComponentInChildren<Text>().text = inputDevice.displayName;
+        var index = System.Array.IndexOf(AvailableInputDevices, inputDevice);
+        Debug.Log("Current index: " + index + " Device: " + inputDevice.displayName + " Player: " + playerNumber + " ToggleGroup: " + toggleGroup.name);
+        toggle.SetActive(true);
+        toggle.transform.localPosition = new Vector2(0, -30 * index);
+        toggle.GetComponent<Toggle>().onValueChanged.AddListener((value) => OnToggleValueChanged(index, playerNumber, value));
     }
 
     private void BlockTogglesUpdate(int playerNumber)
@@ -151,19 +221,7 @@ public class LocalMultiplayerControllerSelection : MonoBehaviour
         }
         BlockTogglesUpdate(playerNumber);
     }
-//     NullReferenceException: Object reference not set to an instance of an object
-// LocalMultiplayerControllerSelection.BlockTogglesUpdate (System.Int32 playerNumber) (at Assets/Scripts/LocalMultiplayerControllerSelection.cs:122)
-// LocalMultiplayerControllerSelection.OnToggleValueChanged (System.Int32 index, System.Int32 playerNumber, System.Boolean isOn) (at Assets/Scripts/LocalMultiplayerControllerSelection.cs:145)
-// LocalMultiplayerControllerSelection+<>c__DisplayClass9_1.<UpdateToggleGroupOptions>b__0 (System.Boolean value) (at Assets/Scripts/LocalMultiplayerControllerSelection.cs:109)
-// UnityEngine.Events.InvokableCall`1[T1].Invoke (T1 args0) (at <30adf90198bc4c4b83910c6fb1877998>:0)
-// UnityEngine.Events.UnityEvent`1[T0].Invoke (T0 arg0) (at <30adf90198bc4c4b83910c6fb1877998>:0)
-// UnityEngine.UI.Toggle.Set (System.Boolean value, System.Boolean sendCallback) (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/Toggle.cs:284)
-// UnityEngine.UI.Toggle.set_isOn (System.Boolean value) (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/Toggle.cs:247)
-// UnityEngine.UI.Toggle.InternalToggle () (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/Toggle.cs:317)
-// UnityEngine.UI.Toggle.OnPointerClick (UnityEngine.EventSystems.PointerEventData eventData) (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/UI/Core/Toggle.cs:328)
-// UnityEngine.EventSystems.ExecuteEvents.Execute (UnityEngine.EventSystems.IPointerClickHandler handler, UnityEngine.EventSystems.BaseEventData eventData) (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/EventSystem/ExecuteEvents.cs:57)
-// UnityEngine.EventSystems.ExecuteEvents.Execute[T] (UnityEngine.GameObject target, UnityEngine.EventSystems.BaseEventData eventData, UnityEngine.EventSystems.ExecuteEvents+EventFunction`1[T1] functor) (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/EventSystem/ExecuteEvents.cs:272)
-// UnityEngine.EventSystems.EventSystem:Update() (at ./Library/PackageCache/com.unity.ugui@1.0.0/Runtime/EventSystem/EventSystem.cs:530)
+
     private static string GetNumbers(string input)
     {
         return new string(input.Where(c => char.IsDigit(c)).ToArray());
